523cd58ba47ba497aea1f0f4b6a483c1cc510ca1
Marcus Zanini <zaninimarcus@hotmail.com>
2026-03-04 00:21:05 -0300

    core: better auth e scalar

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ import {
   jsonSchemaTransform,
   createJsonSchemaTransform,
 } from "fastify-type-provider-zod";
-import fastify from "fastify";
 import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUI from "@fastify/swagger-ui";
 import z from "zod";
-const port = Number(process.env.PORT);
+import { auth } from "./lib/auth.js";
+import cors from "@fastify/cors";
+import fastifyApiReference from "@scalar/fastify-api-reference";
+
+const PORT = Number(process.env.PORT);
 
 const app = Fastify({
   logger: true,
@@ -34,15 +36,45 @@ await app.register(fastifySwagger, {
     servers: [
       {
         description: "Localhost",
-        url: `http://localhost:${port}`,
+        url: `http://localhost:${PORT}`,
       },
     ],
   },
   transform: jsonSchemaTransform,
 });
 
-await app.register(fastifySwaggerUI, {
+await app.register(cors, {
+  origin: [`http://localhost:${PORT}`],
+  credentials: true,
+});
+
+await app.register(fastifyApiReference, {
   routePrefix: "/docs",
+  configuration: {
+    sources: [
+      {
+        title: "Gerenciamento de Treinos API",
+        slug: "gerenciamento-treino-api",
+        url: "/swagger.json",
+      },
+      {
+        title: "Auth API",
+        slug: "auth-api",
+        url: "/api/auth/open-api/generate-schema",
+      },
+    ],
+  },
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/swagger.json",
+  schema: {
+    hide: true,
+  },
+  handler: async (req, res) => {
+    return app.swagger();
+  },
 });
 
 app.withTypeProvider<ZodTypeProvider>().route({
@@ -64,9 +96,46 @@ app.withTypeProvider<ZodTypeProvider>().route({
   },
 });
 
+// Register authentication endpoint
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      // Construct request URL
+      const url = new URL(request.url, `http://${request.headers.host}`);
+
+      // Convert Fastify headers to standard Headers object
+      const headers = new Headers();
+      Object.entries(request.headers).forEach(([key, value]) => {
+        if (value) headers.append(key, value.toString());
+      });
+      // Create Fetch API-compatible request
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+      // Process authentication request
+      const response = await auth.handler(req);
+      // Forward response to client
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error("Erro ao se autenticar");
+      app.log.error(error);
+      reply.status(500).send({
+        error: "Internal authentication error",
+        code: "AUTH_FAILURE",
+      });
+    }
+  },
+});
+
 // Run the server!
 try {
-  await app.listen({ port: port || 3000 });
+  await app.listen({ port: PORT || 3000 });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { openAPI } from "better-auth/plugins";
+
+const PORT = process.env.PORT;
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+export const auth = betterAuth({
+  trustedOrigins: [`http://localhost:${PORT}`],
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  plugins: [openAPI()],
+});
