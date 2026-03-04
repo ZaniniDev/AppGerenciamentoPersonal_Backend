8ce7b1b24d0634dcfa9907b5853e2ffdbae4c726
Marcus Zanini <zaninimarcus@hotmail.com>
2026-03-03 21:41:28 -0300

    core: add fastify e zod provier

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,46 @@
-console.log("Hellow Worlda 2");
-export {};
+import "dotenv/config";
+import Fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+// Declare a route
+app.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/schema",
+  schema: {
+    description: "Rota Hellow World",
+    tags: ["Hello World"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: (req, res) => {
+    return {
+      message: "Hellow World",
+    };
+  },
+});
+
+// Run the server!
+try {
+  await app.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
